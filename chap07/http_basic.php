252158e5fe69b33af6e26f915c254a14bcfb6dc2
Shusuke Nakamura <shuchan.galaxy@gmail.com>
2021-08-01 19:58:58 +0900

    chap07の学習完了

---
+++ chap07/http_basic.php
@@ -0,0 +1,9 @@
+<?php
+require '../vendor/autoload.php';
+
+$cli = new GuzzleHttp\Client([
+    'base_uri' => 'https://wings.msn.to/',
+]);
+
+$res = $cli->request('get', '/tmp/sample.txt');
+print $res->getBody();
