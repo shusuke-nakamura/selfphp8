fa14036726cd29a696c0963130493445c89a730e
Shusuke Nakamura <shuchan.galaxy@gmail.com>
2021-08-07 17:25:24 +0900

    リスト8.12まで学習完了

--- chap08/check2.php
+++ chap08/check2.php
@@ -1,4 +1,6 @@
-<?php require_once '../Encode.php' ?>
+<?php
+require_once '../Encode.php';
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -6,11 +8,11 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>ポストデータ</title>
+    <title>チェックボックス</title>
 </head>
 
 <body>
-    こんにちは、<?= e($_POST['name']) ?>さん！
+    選択されたのは、<?= e(implode(',', ($_POST['lang']))) ?>です。
 </body>
 
 </html>
--- chap08/link1.php
+++ chap08/link1.php
@@ -1,4 +1,3 @@
-<?php require_once '../Encode.php' ?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -6,11 +5,11 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>ポストデータ</title>
+    <title>クエリ情報</title>
 </head>
 
 <body>
-    こんにちは、<?= e($_POST['name']) ?>さん！
+    <a href="link2.php?keyword=<?= urlencode('クエリ情報(&%)') ?>">結果を確認</a>
 </body>
 
 </html>
