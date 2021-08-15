413d3a5d0223615c0973def3f557297c035bbdca
Shusuke Nakamura <shuchan.galaxy@gmail.com>
2021-08-15 17:30:54 +0900

    リスト10.38まで学習完了

---
+++ chap10/practice/p_inherit.php
@@ -0,0 +1,15 @@
+<?php
+class MyClass
+{
+    protected string $data;
+
+    public function __construct(string $data)
+    {
+        $this->data = $data;
+    }
+
+    public function getData(): string
+    {
+        return $this->data;
+    }
+}
