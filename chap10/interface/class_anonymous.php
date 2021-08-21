3f4a692335c8c288565ba494f320ab90ad569238
Shusuke Nakamura <shuchan.galaxy@gmail.com>
2021-08-21 10:36:17 +0900

    リスト10.49まで学習完了

---
+++ chap10/interface/class_anonymous.php
@@ -0,0 +1,25 @@
+<?php
+
+interface Runnable
+{
+    function run();
+}
+
+class MyClass
+{
+    public function execute(Runnable $rc): void
+    {
+        print 'start...';
+        $rc->run();
+        print 'end...';
+    }
+}
+
+$cls = new MyClass();
+$cls->execute(new class implements Runnable
+{
+    public function run()
+    {
+        print 'process...';
+    }
+});
