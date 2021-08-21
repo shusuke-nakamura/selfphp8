bcb289b3f7c679fa8b923267cff10fd4f85f7d34
Shusuke Nakamura <shuchan.galaxy@gmail.com>
2021-08-21 11:45:45 +0900

    10.53まで学習完了

---
+++ chap10/trait_alias.php
@@ -0,0 +1,28 @@
+<?php
+trait MyTrait1
+{
+    public function hoge(): void
+    {
+        print 'MyTrait1';
+    }
+}
+
+trait MyTrait2
+{
+    public function hoge(): void
+    {
+        print 'MyTrait2';
+    }
+}
+
+class MyClass
+{
+    use MyTrait1, MyTrait2 {
+        MyTrait1::hoge insteadof MyTrait2;
+        MyTrait2::hoge as foo;
+    }
+}
+
+$cls = new MyClass();
+$cls->hoge();
+$cls->foo();
