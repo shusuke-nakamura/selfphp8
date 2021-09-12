45ca3d32a9c6d0dcd52d77263752b7f4e046a2a7
Shusuke Nakamura <shuchan.galaxy@gmail.com>
2021-09-12 20:37:25 +0900

    chap11の学習完了

---
+++ chap11/mail_send.php
@@ -0,0 +1,7 @@
+<?php
+if (!preg_match('/\A\w+([-+.\']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*\z/i', $_POST['from'])) {
+    die('不正なメールアドレスです。');
+}
+
+mb_send_mail('shuchan.galaxy@gmail.com', $_POST['subject'], $_POST['body'], "From: {$_POST['from']}");
+print 'メールが送信されました。';
---
+++ chap11/null_byte.php
@@ -0,0 +1,8 @@
+<?php
+function sanitizeNull(array $data): array {
+    return str_replace("\0", "", $data);
+}
+
+$_GET = sanitizeNull($_GET);
+$_POST = sanitizeNull($_POST);
+$_COOKIE = sanitizeNull($_COOKIE);
